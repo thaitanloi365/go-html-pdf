861efea41a0e8702b135252494e148a5ba22c508
Thái Tấn Lợi <thaitanloi365@gmail.com>
2020-12-26 22:51:23 +0700

    first draft version

--- main.go
+++ main.go
@@ -8,28 +8,90 @@ import (
 	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
 )
 
+type InvoiceBillingData struct {
+	ContactName string
+	CompanyName string
+	Address     string
+	Phone       string
+	Email       string
+}
+type InvoiceItem struct {
+	Description string
+	Quantity    string
+	UnitPrice   string
+	Total       string
+	IsLast      bool
+}
+
+type InvoiceData struct {
+	InvoiceDate              string
+	InvoiceNumber            string
+	Billing                  InvoiceBillingData
+	Shipping                 InvoiceBillingData
+	Items                    []InvoiceItem
+	SubTotal                 string
+	Discount                 string
+	SubTotalLessThanDiscount string
+	TaxRate                  string
+	TotalTax                 string
+	ShippingOrHandlingFee    string
+	BalanceDue               string
+}
+
 func main() {
 	//html template path
-	templatePath := "sample.html"
+	var templatePath = "sample.html"
 
 	//path for download pdf
-	outputPath := "example.pdf"
+	var outputPath = "example.pdf"
 
 	//html template data
-	templateData := struct {
-		Title       string
-		Description string
-		Company     string
-		Contact     string
-		Country     string
-	}{
-		Title:       "HTML to PDF generator",
-		Description: "This is the simple HTML to PDF file.",
-		Company:     "Jhon Lewis",
-		Contact:     "Maria Anders",
-		Country:     "Germany",
+	var templateData = InvoiceData{
+		InvoiceDate:   "1/1/2021",
+		InvoiceNumber: "123456789",
+		Billing: InvoiceBillingData{
+			ContactName: "Loi",
+			CompanyName: "Relia",
+			Address:     "95 Nguyen Tat Thanh",
+			Phone:       "+123456789",
+			Email:       "thaitanloi365@gmail.com",
+		},
+		Shipping: InvoiceBillingData{
+			ContactName: "Loi 2",
+			CompanyName: "Relia",
+			Address:     "607 Nguyen Kiem",
+			Phone:       "+123456789",
+			Email:       "thaitanloi365@gmail.com",
+		},
+		Items: []InvoiceItem{
+			{
+				Description: "Label - NAME OF 3PL - DATE",
+				Quantity:    "x1",
+				UnitPrice:   "$20",
+				Total:       "$20",
+			},
+			{
+				Description: "Label - NAME OF 3PL - DATE",
+				Quantity:    "x1",
+				UnitPrice:   "$20",
+				Total:       "$20",
+			},
+			{
+				Description: "Label - NAME OF 3PL - DATE",
+				Quantity:    "x1",
+				UnitPrice:   "$20",
+				Total:       "$20",
+				IsLast:      true,
+			},
+		},
+		SubTotal:                 "$10",
+		Discount:                 "$10",
+		SubTotalLessThanDiscount: "$10",
+		TaxRate:                  "$10",
+		TotalTax:                 "$10",
+		ShippingOrHandlingFee:    "$10",
+		BalanceDue:               "$10",
 	}
-
 	t, err := template.ParseFiles(templatePath)
 	if err != nil {
 		panic(err)
